e287e363aa7a9da32b27f19f5d6f7b30df8698c5
Jon Parise <jon@indelible.org>
2012-10-25 14:16:55 -0700

    Import the correct header file name.

--- AFHTTPClientLogger.m
+++ AFHTTPClientLogger.m
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "AFHTTPClient+Logging.h"
+#import "AFHTTPClientLogger.h"
 #import "AFHTTPRequestOperation.h"
 
 #import <objc/runtime.h>
