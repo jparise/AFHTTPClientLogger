98729067d93f1328186fd23223df054942a31c7d
Jon Parise <jon@indelible.org>
2016-02-09 17:14:48 -0800

    Update copyright range to include 2016.

--- AFHTTPClientLogger.h
+++ AFHTTPClientLogger.h
@@ -1,6 +1,6 @@
 // AFHTTPClientLogger.h
 //
-// Copyright (c) 2012-2015 Jon Parise
+// Copyright (c) 2012-2016 Jon Parise
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
